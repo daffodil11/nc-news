0b9e44334216f344a45478ac4a1d5f7e6748efa5
Simon Davey <simonr.davey@gmail.com>
2019-06-28 13:58:44 +0100

    LICENSE and README.

--- connection.js
+++ connection.js
@@ -2,10 +2,10 @@ const ENV = process.env.NODE_ENV || 'development';
 const types = require('pg').types;
 types.setTypeParser(20, 'text', parseInt);
 const Knex = require('knex');
-const herokuConfig = {
+const hostingConfig = {
     client: 'pg',
     connection: process.env.DATABASE_URL
 };
-const config = (ENV === 'production') ? herokuConfig : require('./knexfile');
+const config = (ENV === 'production') ? hostingConfig : require('./knexfile');
 
 module.exports = Knex(config);

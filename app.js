18dbbf007acd53378a610704ff9b6142aab6ea0c
Simon Davey <simonr.davey@gmail.com>
2019-07-15 14:47:04 +0100

    Adds cors middleware.

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const apiRouter = require('./routes/api');
 const { handle500Errors, handleCustomErrors, handlePSQL400Errors } = require('./errors');
+const cors = require('cors');
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/api', apiRouter);
 app.use('/*', (req, res, next) => res.status(404).send({ msg: 'Not found'}));
